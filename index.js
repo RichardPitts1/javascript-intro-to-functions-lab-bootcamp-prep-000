ef8fdc94977daefb0d978a47605cf0168c918418
Richard Pitts <richardepitts@gmail.com>
2019-06-29 13:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
 }
@@ -27,8 +27,8 @@ function sayHiToGrandma(string) {
   }
 }
 
-// function sayHiToGrandma(string) {
-//   if (string === "I love you, too") {
-//     return "I love you, too";
-//   }
-// }
+function sayHiToGrandma(string) {
+  if (string === "I love you, too") {
+    return "I love you, too";
+  }
+}
