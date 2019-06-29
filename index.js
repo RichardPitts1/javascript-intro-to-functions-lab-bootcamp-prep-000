76c9a4dc2343c6c89b949660a30f378edf25e766
Richard Pitts <richardepitts@gmail.com>
2019-06-29 13:27:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+  var text = string
+  if (text.toLowerCase() === lowercase) {
     return "I can't hear you!";
   }
 }
